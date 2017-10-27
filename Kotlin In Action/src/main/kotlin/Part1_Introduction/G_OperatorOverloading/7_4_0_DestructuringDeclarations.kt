4d960e0b995b0e819192435dea8ef5cf59fdfc00
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2017-10-26 23:47:52 -0400

    KIA: destructuring and componentN values, from functions, maps and more

--- Kotlin In Action/src/main/kotlin/Part1_Introduction/G_OperatorOverloading/7_4_0_DestructuringDeclarations.kt
+++ Kotlin In Action/src/main/kotlin/Part1_Introduction/G_OperatorOverloading/7_4_0_DestructuringDeclarations.kt
@@ -1,2 +1,38 @@
 package Part1_Introduction.G_OperatorOverloading
 
+// Destructuring declarations and component functions
+fun dest() {
+    val p = Point(1, 2)
+
+    val (x, y) = p
+    println("x:$x and y:$y")
+
+    val x1 = p.component1()
+}
+
+// for each variable in a destructuring declaration a function named componentN is called, where N is the position of the
+// variable in the declaration, thus a = p.component1()
+
+// data classes generate a componentN function for every property declared in the primary constructor
+// they can also be defined manually
+
+operator fun Point.component1() = x
+
+// destructuring is useful to unpack values from a function, also useful if used with arrays and collections of known size
+// you can access the first 5 elements via componentN, or use Pair or Triple for wrapper classes
+
+val m = mapOf(1 to "a", 2 to "b")
+
+fun de() {
+    for ((key, value) in m) {
+        println(key)
+        println(value)
+    }
+
+    // or
+
+    for(entry in m.entries){
+        println(entry.component1())
+        println(entry.component2())
+    }
+}
