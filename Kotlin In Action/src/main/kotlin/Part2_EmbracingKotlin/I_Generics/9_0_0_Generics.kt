34515edd8e6d32138e043f015844a0ff8e6088d8
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2017-11-09 00:01:24 -0500

    Kia: labels and anon functions(returning too)

--- Kotlin In Action/src/main/kotlin/Part2_EmbracingKotlin/I_Generics/9_0_0_Generics.kt
+++ Kotlin In Action/src/main/kotlin/Part2_EmbracingKotlin/I_Generics/9_0_0_Generics.kt
@@ -5,15 +5,16 @@ package Part2_EmbracingKotlin.I_Generics
 // type erasure and reified type parameters
 // Declaration site and use site variance
 
-// Type arguments for genirc types only exist at compilet time
+// Type arguments for generic types only exist at compile time
 // can't use types with type arguments together with the 'is' operator, because type arguments are erased at runtime
 // Type parameters of inline functions can be marked as reified to allow you to use them at runtime to perform 'is'
 // checks and obtain java.lang.Class instances
 // Variance is a way to specify whether a type parameter of a type can be substituted for its sub or super class
 // can declare a class as covariant on a type parameter if the parameter used only in 'out' position
-// oposite is true for contravariant cases, for in positions
+// opposite is true for contravariant cases, for in positions
 // read only interface List in kotlin is covariant so List<String> is a subtype of List<Any)
 // function interface is declared as contravariant on its first type param and covariant on second, so (Animal) -> Int
 // is a subtype of (Cat) -> Number
-// Kotlin lets yoau specify variance both for a generic class as a whole(declaration-site variance and generic type(use syte variance)
-// stare propection syntax can be used when the exact type args are unkonown
+// Kotlin lets you specify variance both for a generic class as a whole(declaration-site variance and
+// generic type(use site variance)
+// star projection syntax can be used when the exact type args are unknown or unimportant
