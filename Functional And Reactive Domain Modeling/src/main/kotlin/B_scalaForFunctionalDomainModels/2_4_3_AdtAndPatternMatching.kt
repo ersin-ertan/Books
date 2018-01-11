bae2a3b31cf8726f60168387ea08c581cda9399d
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2018-01-10 21:25:28 -0500

    FRDM Pattern matching and functional small oo large

--- Functional And Reactive Domain Modeling/src/main/kotlin/B_scalaForFunctionalDomainModels/2_4_3_AdtAndPatternMatching.kt
+++ Functional And Reactive Domain Modeling/src/main/kotlin/B_scalaForFunctionalDomainModels/2_4_3_AdtAndPatternMatching.kt
@@ -6,7 +6,84 @@ package B_scalaForFunctionalDomainModels
 
 class Ex06 {
 
-    class Balance(val amount: Int = 0)
+    fun getMarketValue(e: Equity, a: Int): Amount = Amount(a, Currency.USD)
+    fun getAccruedInterest(i: String): Amount = Amount(1, Currency.USD)
+
+    class Balance(val amount: Int = 0, val ins: Instrument, val asOf: Int)
 
     class Account(val no: String, val name: String, val dateOfOpening: Int, val balance: Balance)
+
+    interface Instrument
+
+    class Equity(isin: String, name: String, dateOfIssue: Int, issueCurrency: Currency, nominal: Int) : Instrument
+    class FixedIncome(val isin: String, val name: String, val dateOfIssue: Int, val issueCurrency: Currency, val nominal: Int) : Instrument
+
+    sealed class Currency : Instrument {
+        object USD : Currency()
+        object CAD : Currency()
+    }
+
+    class Amount(val amount: Int, val currency: Currency) {
+
+        operator fun plus(that: Amount): Amount {
+            require(that.currency == currency)
+            return Amount(amount + that.amount, currency)
+        }
+    }
+
+    val getMarketValue: (e: Equity) -> Amount = { Amount(1, Currency.USD) }
+    val getAccruedInterest: (i: String) -> Amount = { Amount(1, Currency.USD) }
+
+    val getHolding: (account: Account) -> Amount = { account ->
+        with(account.balance) {
+            when (ins) { // equivalent to pattern matching
+                is Currency -> Amount(amount, ins)
+                is Equity -> getMarketValue(ins, amount)
+                is FixedIncome -> Amount(ins.nominal * amount, ins.issueCurrency).plus(getAccruedInterest(ins.isin))
+                else -> {
+                    Amount(1, Currency.USD)
+                } // were these sealed? placeholder return
+            }
+        }
+    }
+
+    // pattern matching in getHolding computing the accounts net holding value based on the type of balance that it holds
+    // Instrument is defined as a sum of product type, pattern matching enables localization of logic with sum type and
+    // pattern matching.  Using Visitor pattern(hard to extend) algebraic data types solve the problem elegantly. Compiler
+    // checks for all enumerations of balance
+}
+
+// 2.4.4 ADTs encourage immutability
+/*
+Referential transparency: immutable thus always have a value be understood as our structure shows.
+How do we modify the value of any of its attributes? Create a new instance with the modified value.
+
+Share values without mutable state, keep domain behaviours pure.
+*/
+
+fun immutableClasses() {
+    class SavingsAccount(val number: String, val name: String, val dateOfOpening: Int, val rateOfInterest: Int) {
+        fun copy(number: String = this.number,
+                 name: String = this.name,
+                 dateOfOpening: Int = this.dateOfOpening,
+                 rateOfInterest: Int = this.rateOfInterest) = SavingsAccount(number, name, dateOfOpening, rateOfInterest)
+
+    }
+
+    val today = 1
+
+    val a1 = SavingsAccount("a-234", "tom", today, 3)
+    val a2 = a1.copy(rateOfInterest = 5)
+
+    class B(val valB: Int) {
+        fun copy(b: Int = this.valB) = B(b)
+    }
+
+    class A(val valA: Int, val classB: B) {
+        fun copy(valA: Int = this.valA, classB: B = this.classB) = A(valA, classB)
+    }
+    // what about nested copy? Inner double copy
+
+    val a = A(1, B(2))
+    val aa = a.copy(classB = a.classB.copy(3))
 }
---
+++ Functional And Reactive Domain Modeling/src/main/kotlin/B_scalaForFunctionalDomainModels/2_5_0_FunctionalSmallOOLarge.kt
@@ -0,0 +1,111 @@
+package B_scalaForFunctionalDomainModels
+
+import kategory.Option
+import kategory.Some
+
+// 2.5.0 Functional in the small, OO in the large
+
+/*
+Non trivial domain model has many abstractions, entities, value objects, services, and associated behaviours.
+Monolithic app, bundled in same namespace has entanglement. No context bound separation. Need modules, they must be
+highly cohesive but loosely coupled, abstractions small and tight.
+
+Scala uses traits(mixin based composition) and objects. Not function composition, but unit of functionality, with few methods
+delivering functionality
+*/
+
+
+class Ex07 {
+
+    interface IBalance
+    interface IInterestCalc
+    interface ITaxCalc // defined below
+    interface ILogging
+
+    interface PortfolioGeneration : IBalance, IInterestCalc, ITaxCalc, ILogging // {... implementations}
+
+    // all orthogonal to each other, can be reused in other contexts
+
+    object PortfolioGenerator : PortfolioGeneration // but why have interface PortfolioGenereration? why not directly in Generator
+    // have final module in form of trait before committing to concrete implementation
+
+    // then when you need a bigger abstraction, you can define it composed with the PortfolioGeneration trait
+
+    // Module implements specific business functionality, one module is usually part of one bounded context where a
+    // bounded context may contain many modules.
+
+    // Modules can be parameratized to incorporate variations in business rules
+    // For specific date range: Calc interest, calc tax deducted from interest, tax needs specific tax tables of item and rate
+    // depending on transaction
+
+    // Module computing net interest referes to another module for computation of tax deduction(which is parameterized on
+    // text tables that depend on the transaction type being performed
+
+    sealed class TaxType {
+
+        object Tax : TaxType()
+        object Fee : TaxType()
+        object Commission : TaxType()
+
+    }
+
+    sealed class TransactionType {
+        object InteresteComputation : TransactionType()
+        object Dividend : TransactionType()
+    }
+
+    class Balance(val amount: Int = 0)
+
+    interface ITaxCalculationTable<TT : TransactionType> {
+        val transactionType: TT
+
+        fun getTaxRates(): Map<TaxType, Int> = mapOf(TaxType.Tax to 1)
+    }
+
+    interface ITaxCalculation<TCT : ITaxCalculationTable<TransactionType>> {
+        val table: TCT
+        fun calculate(taxOn: Int): Int = table.getTaxRates().map { doCompute(taxOn, it.value) }.sum()
+        private fun doCompute(taxOn: Int, rate: Int) = taxOn * rate
+    }
+
+    interface ISingaporeTaxCalc : ITaxCalculation<ITaxCalculationTable<TransactionType>> {
+        fun calculateGST(tax: Int, gstRate: Int) = tax * gstRate
+    }
+
+    interface IInterestCalculation<TC : ITaxCalculation<ITaxCalculationTable<TransactionType>>> {
+        val taxCalculation: TC
+
+        fun interest(b: Balance): Option<Int> = Some(b.amount * 1)
+        fun calculate(b: Balance): Option<Int> = interest(b).map { it - taxCalculation.calculate(it) }
+    }
+
+    // no contrete instances of modules, just module definitions and dependencies reflecting variability in domain model
+    // Implement variablities using abstract types and vals
+
+    // concrete module for interest calculation, don't need to commit to the exact type or value until you declare the object
+
+    object InterestTaxCalculationTable : ITaxCalculationTable<TransactionType> {
+        override val transactionType: TransactionType
+            get() = TransactionType.InteresteComputation
+
+    }
+
+    object TaxCalculation : ITaxCalculation<ITaxCalculationTable<TransactionType>> {
+        override val table: ITaxCalculationTable<TransactionType>
+            get() = InterestTaxCalculationTable
+    }
+
+    object InterestCalculation : IInterestCalculation<ITaxCalculation<ITaxCalculationTable<TransactionType>>> {
+        override val taxCalculation: ITaxCalculation<ITaxCalculationTable<TransactionType>>
+            get() = TaxCalculation
+    }
+}
+
+// InterestCalc, TaxCalcu, TaxCalcTable all have parameterized module contracts with TaxCalc, TaxCalcTable, and transactionType
+// respectfully to form a dependency chain. Each of the contrete modules then have their respective contrete implmentation
+// dependency chains and parameterized module contracts(InterestComp, InterestTaxCalcTable, TaxCalc)
+
+
+// Successful domain modeling requires: static type systems(rich types system, type inference, higher order(kinds)type support)
+// Functional programming: algebraic data types, pattern matching, pure functions, higher order functions, compositionality, adv. functional design patterns
+// Powerful module System: first class module support, composable modules, parameterized modules
