4e95b30781ff1457565b888adf31616bbb00fbf3
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2018-01-11 23:51:14 -0500

    FRDM Start making models reactive

---
+++ Functional And Reactive Domain Modeling/src/main/kotlin/B_scalaForFunctionalDomainModels/2_6_0_MakingModelsReactive.kt
@@ -0,0 +1,30 @@
+package B_scalaForFunctionalDomainModels
+
+// 2.6.0 Making modes reactive with scala
+
+// Functional thinking and implementing with pure functions for domain models is the engineering
+// Need language support to build models that are: responsive to failures, scales well with increasing load, fast ux
+// manage failures(design for failure), minimize latency delegate long running processes to background threads
+
+// manage latency and exceptions as effects, composed along with other pure abstraction of your domain model
+// effect adds capabilities to your computation so you don't need to use side effects to model them. Effectful computation
+
+// Effectful computation - give you power via the type system, effects is modeled in form of type constructor constructing
+// constructing types with additional capabilities. Ex type A to add aggregation, construct type List<A>, or Option<A>
+// adds optionallity for the type A. Ex Try, Future to model effects of exceptions and latency, respectfully. More advance
+// is applicatives and monads.
+
+// 2.6.1 Managing Effects
+
+// How to adapt concepts to functional programming, treat them as part of the model so that they can be composed in a
+// referentially transparent way, along with other domain elements. Effect in the sense that you abstract them within containers
+// that expose functional interfaces to the world.
+
+// Monad comes from category theory, but here it is an abstract computation helps mimic effects of impure actions like
+// exceptions, IO, continuations, while providing a functional interface that composes with other components.
+
+// 2.6.2 Managing Failures
+// Failures will happen: hardware, network, third party software, own components.
+// What models do we use, littering code with error checks is not a solution. Make code that raises exceptions explicit
+// via type system, and and use abstractions taht don't leak exception management details within your domain logic leaving
+// core logic functionally compositional.
