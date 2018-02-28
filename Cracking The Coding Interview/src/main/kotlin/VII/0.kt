7f8ce8a73bcfca749dc887662447cf92c5877413
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2018-02-28 09:38:40 -0500

    Cracking the coding interview template files

---
+++ Cracking The Coding Interview/src/main/kotlin/VII/0.kt
@@ -0,0 +1,30 @@
+package VII
+
+// Practise every day, think about space and time efficiency
+// Code on paper
+// Test code on paper
+// Put paper into code 1:1 to verify
+
+// Must have:
+// linked list, trees, tries, graphs, stacks, queues, heaps, vectors, arraylists, hashtabes
+// Breadth first search, depth first search, binary search, merge sort, quick sort, (cassowary?)
+// bit manipulation, memory(stack heap), recursion, dynamic programing, big O time and space
+
+// Power of 2
+// 7(128), 8(256), 10(1024) 1KB, 16(65,536) 64KB, 20(1,048, 576) 1MB, 30(1,073,741,824) 1GB
+
+// Walking through a problem:
+// listen and look at description, examples may be to smmall or special cases, debug it, brute force solution, optimize
+// bottlenecks, usless work, duplicated work, walk through optimal solution, implement, test
+
+// Bottlenecks: if the outer loop is not optimized then the inner will eat it
+
+// Consider the operations, and consider which data structure suits it best
+
+// Best Conceivable Runtime(BCR) Best and can prove it, has nothing to do with implementation
+
+// Is working with sorted data helpful for the solution
+
+// Negotiate with exact figures and numbers, ask for what you want, advance your career
+
+// www.crackingthecodinginterview.com for complete solutions
