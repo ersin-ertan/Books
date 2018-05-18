d6016f56c361fc63b025daceb44bdf573a3b276c
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2018-05-17 23:59:12 -0400

    Functional Kotlin: Start ch05: Inline

--- Functional Kotlin/src/main/kotlin/Ch05/03_InlineFunctions.kt
+++ Functional Kotlin/src/main/kotlin/Ch05/03_InlineFunctions.kt
@@ -1,2 +1,7 @@
 package Ch05
 
+// Inline functions - higherder functions coe with caveat performance penalties, on copileation time, lambda gets translated
+// to an object that is allocated, calling invoke operator
+
+// for performance critical locations mark fun with inline and the function execution is replaced by the higher order functions
+// body and the lambdas body, faster but at the cost of more byte code, execution optimizations compound
