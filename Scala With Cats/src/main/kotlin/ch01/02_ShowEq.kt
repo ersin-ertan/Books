99bbe644ce70e801696a65ce4f7c6a2f201178b0
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2018-06-02 22:10:58 -0400

    SwC: Finished: ReaderMonad

--- Scala With Cats/src/main/kotlin/ch01/02_ShowEq.kt
+++ Scala With Cats/src/main/kotlin/ch01/02_ShowEq.kt
@@ -25,13 +25,13 @@ fun main(args: Array<String>) {
 //    testingImplicits()
 //    testingSyntax()
 //    showEx()
-    comparingWithEq()
+  comparingWithEq()
 }
 
 
 fun testingImplicits() {
-    val a = JsonInterfaceObject.toJson(Person("dave", "dave@gmail.com"), JsonWriterInstances.personWriter)
-    println(a)
+  val a = JsonInterfaceObject.toJson(Person("dave", "dave@gmail.com"), JsonWriterInstances.personWriter)
+  println(a)
 }
 
 // kotlin does not have implicits thus, we must always provide an Instance
@@ -39,18 +39,18 @@ fun testingImplicits() {
 // Interface syntax - or use extension method to existing types with interface methods
 
 object JsonSyntax {
-    class JsonWriterOps<A>(val value: A) {
-        fun A.toJson(w: JsonWriter<A>): Json = w.write(value)
-    }
+  class JsonWriterOps<A>(val value: A) {
+    fun A.toJson(w: JsonWriter<A>): Json = w.write(value)
+  }
 }
 
 fun testingSyntax() {
-    // NoSuchMehodException... thus JsonSyntax doesn't work
+  // NoSuchMehodException... thus JsonSyntax doesn't work
 //    println(JsonSyntax.JsonWriterOps<Person>(Person("a", "b")).javaClass.getDeclaredField("value").type.getDeclaredMethod("toJson", JsonWriterInstances.personWriter.javaClass).invoke(Person("a", "b"), JsonWriterInstances.personWriter).toString())
 //    Person("Dave", "da@g.com").toJson
 //    Person("dave", "d@id.co").toJson(personWriter) // again doesn't work unless you make an extension function for all objects
-    fun Person.toJson(w: JsonWriter<Person>): Json = w.write(this)
-    Person("d", "d@D.d").toJson(JsonWriterInstances.personWriter)
+  fun Person.toJson(w: JsonWriter<Person>): Json = w.write(this)
+  Person("d", "d@D.d").toJson(JsonWriterInstances.personWriter)
 }
 
 // implicitly method - generic type class interface, to get the value from implicit scope
@@ -62,23 +62,23 @@ fun testingSyntax() {
 // 1.3 Exercise: Printable Library - define Printable type class to work around:
 // 1. Define a type class Printable<A> containing a single method format accepting value of type a and return string
 interface Printable<A> {
-    fun format(value: A): String
+  fun format(value: A): String
 }
 
 // 2. create an object PrintableInstances containing instances of Printable fo String and Int
 object PrintableInstances {
-    val stringPrinter = object : Printable<String> {
-        override fun format(value: String): String = value
+  val stringPrinter = object : Printable<String> {
+    override fun format(value: String): String = value
 
-    }
-    val intPrinter = object : Printable<Int> {
-        override fun format(value: Int): String = value.toString()
-    }
+  }
+  val intPrinter = object : Printable<Int> {
+    override fun format(value: Int): String = value.toString()
+  }
 }
 
 object PrintableInterfaceObject {
-    fun <A> format(value: A, printable: Printable<A>): String = printable.format(value)
-    fun <A> print(value: A, printable: Printable<A>): Unit = println(printable.format(value))
+  fun <A> format(value: A, printable: Printable<A>): String = printable.format(value)
+  fun <A> print(value: A, printable: Printable<A>): Unit = println(printable.format(value))
 }
 
 // solution
@@ -86,25 +86,25 @@ object PrintableInterfaceObject {
 class Cat(val name: String, val age: Int, val color: String)
 
 fun solution() {
-    val cat = Cat("Scratchy", 1, "Brown")
+  val cat = Cat("Scratchy", 1, "Brown")
 
-    val catPrintable = object : Printable<Cat> {
-        override fun format(value: Cat): String {
-            val name = PrintableInterfaceObject.format(value.name, PrintableInstances.stringPrinter)
-            val age = PrintableInterfaceObject.format(value.age, PrintableInstances.intPrinter)
-            val color = PrintableInterfaceObject.format(value.color, PrintableInstances.stringPrinter)
-            return "$name is $age year-old $color value"
-        }
+  val catPrintable = object : Printable<Cat> {
+    override fun format(value: Cat): String {
+      val name = PrintableInterfaceObject.format(value.name, PrintableInstances.stringPrinter)
+      val age = PrintableInterfaceObject.format(value.age, PrintableInstances.intPrinter)
+      val color = PrintableInterfaceObject.format(value.color, PrintableInstances.stringPrinter)
+      return "$name is $age year-old $color value"
     }
+  }
 
-    PrintableInterfaceObject.print(cat, catPrintable)
+  PrintableInterfaceObject.print(cat, catPrintable)
 
 //    or
 
-    fun Cat.format(catPrintable: Printable<Cat>): String = catPrintable.format(this)
-    fun Cat.print(catPrintable: Printable<Cat>): Unit = println(catPrintable.format(this))
+  fun Cat.format(catPrintable: Printable<Cat>): String = catPrintable.format(this)
+  fun Cat.print(catPrintable: Printable<Cat>): Unit = println(catPrintable.format(this))
 
-    cat.print(catPrintable) // instead of using interface objects
+  cat.print(catPrintable) // instead of using interface objects
 }
 
 // Meet cats - how to implement type classes in cats, ex cats.Show
@@ -118,46 +118,41 @@ fun solution() {
 //  Show.apply { Int } // remember you need the instances package, but apply is already used in Kotlin so we use:
 fun showEx() {
 
-    // this might be typealiasing
+  // this might be typealiasing
 //    val showInt:Show<Int> = Show.apply{Int} // now working
 //    val intAsString:String = showInt.show(234)
-    // using invoke and run???????? !! no need to use invoke just use the constructor
-    val showInt = Show.invoke<Int> { it.toString() }
-    val intAsString1 = showInt.run { 234.show() }
-    println("intAsString1 = $intAsString1")
+  // using invoke and run???????? !! no need to use invoke just use the constructor
+  val showInt = Show.invoke<Int> { toString() }
+  val intAsString1 = showInt.run { 234.show() }
+  println("intAsString1 = $intAsString1")
 
 //    can define custom instance of show like so, these are working
-    val dateShow: Show<Date> = object : Show<Date> {
-        override fun Date.show(): String = "${this.time}ms since epoch"
-    }
-    // or constructor lambda
-    val dateShow2: Show<Date> = Show({ "${it.time}ms since epoch" })
+  val dateShow: Show<Date> = object : Show<Date> {
+    override fun Date.show(): String = "${time}ms since epoch"
+  }
+  // or constructor lambda
+  val dateShow2: Show<Date> = Show({ "${time}ms since epoch" })
 
-    val dd = dateShow2.run { Date().show() }
-    val ddd = dateShow.run { Date().show() }
-    println(dd)
-    println(ddd)
+  val dd = dateShow2.run { Date().show() }
+  val ddd = dateShow.run { Date().show() }
+  println(dd)
+  println(ddd)
 
 
-    // but I still don't think this is correct
-    val intShow: Show<Int> = Show { it.toString() }
-    val intAsString = intShow.apply { 234.show() }
+  // but I still don't think this is correct
+  val intShow: Show<Int> = Show { toString() }
+  val intAsString = intShow.apply { 234.show() }
 
-    // try using run
+  // try using run
 
-    // 4 re implement cat using show
-    val catShow = Show<Cat> {
-        val name = it.name
-        val age = it.age
-        val color = it.color
-        "$name is a $age year old $color cat"
-    }
-    val c2 = ch01.Show<Cat> { "$name is a $age year old $color cat" }
+  // 4 re implement cat using show
+  val catShow = Show<Cat> { "$name is a $age year old $color cat" }
+  val c2 = ch01.Show<Cat> { "$name is a $age year old $color cat" }
 
-    val cat = Cat("Tim", 23, "blue")
+  val cat = Cat("Tim", 23, "blue")
 //    cat.show // this doesn't work, unless I create an extension method
 //    fun Cat.show() = "$name is a $age year old $color cat"
-    catShow.run { cat.show() } // but does within the run
+  catShow.run { cat.show() } // but does within the run
 }
 
 // 1.5 Equality - type safe equality to address == operator
@@ -168,57 +163,57 @@ fun showEx() {
 
 fun comparingWithEq() {
 
-    val eqInt = Eq<Int> { a, b -> a == b }
-    eqInt.run { 2.eqv(55) }
-    //or
-    val iei = object : IntEqInstance {}
-    iei.run { 3.eqv(5) }
+  val eqInt = Eq<Int> { a, b -> a == b }
+  eqInt.run { 2.eqv(55) }
+  //or
+  val iei = object : IntEqInstance {}
+  iei.run { 3.eqv(5) }
 
 
-    val catEq = Eq<Cat> { c, cc -> c == cc }
-    val catEq1 = ch01.Eq<Cat> { c, cc -> c.color == cc.color }
+  val catEq = Eq<Cat> { c, cc -> c == cc }
+  val catEq1 = ch01.Eq<Cat> { c, cc -> c.color == cc.color }
 
 //    val isCatEq  = catEq1.run { Cat("2", 2, "3").eqv() } // doesnt work, must have two names, not receiver lambda
 
-    val dateEq: Eq<Date> = Eq { d1, d2 -> d1.time == d2.time }
-    val x = Date()
-    val y = Date()
+  val dateEq: Eq<Date> = Eq { d1, d2 -> d1.time == d2.time }
+  val x = Date()
+  val y = Date()
 
-    val isEq: Boolean = dateEq.run { x.eqv(y) }
+  val isEq: Boolean = dateEq.run { x.eqv(y) }
 //    dateEq.Eq<Int>({ a, b -> a == b })
 
-    // now comparing Option<Int> we need to compare instances of eq for Option and Int
+  // now comparing Option<Int> we need to compare instances of eq for Option and Int
 
-    1.some() // .eqv( ... not yet defined
+  1.some() // .eqv( ... not yet defined
 
-    val intEqInstance = object : IntEqInstance {}
+  val intEqInstance = object : IntEqInstance {}
 
-    val optionEqInstance = object : OptionEqInstance<Int> {
-        override fun EQ(): Eq<Int> = intEqInstance
-    }
+  val optionEqInstance = object : OptionEqInstance<Int> {
+    override fun EQ(): Eq<Int> = intEqInstance
+  }
 
-    println(optionEqInstance.run { 1.some().eqv(none()) })
+  println(optionEqInstance.run { 1.some().eqv(none()) })
 
-    println(optionEqInstance.EQ())
+  println(optionEqInstance.EQ())
 
-    // 1.5.5 cat equality
+  // 1.5.5 cat equality
 
-    val c1 = Cat("A", 2, "brown")
-    val c2 = Cat("B", 2, "teal")
+  val c1 = Cat("A", 2, "brown")
+  val c2 = Cat("B", 2, "teal")
 
-    val optC1 = c1.some()
-    val optC2 = Option.empty<Cat>()
+  val optC1 = c1.some()
+  val optC2 = Option.empty<Cat>()
 
-    val catEqInstance = object : Eq<Cat> {
-        override fun Cat.eqv(b: Cat): Boolean =
-                (name == b.name) && (age == b.age) && (color == b.color)
+  val catEqInstance = object : Eq<Cat> {
+    override fun Cat.eqv(b: Cat): Boolean =
+      (name == b.name) && (age == b.age) && (color == b.color)
 
-    }
+  }
 
-    val catOptionEqInstance = object : OptionEqInstance<Cat> {
-        override fun EQ(): Eq<Cat> = catEqInstance
-    }
+  val catOptionEqInstance = object : OptionEqInstance<Cat> {
+    override fun EQ(): Eq<Cat> = catEqInstance
+  }
 
-    val areOptCatsEquals = catOptionEqInstance.run { optC1.eqv(optC2) }
-    println("OptCats are $areOptCatsEquals")
+  val areOptCatsEquals = catOptionEqInstance.run { optC1.eqv(optC2) }
+  println("OptCats are $areOptCatsEquals")
 }
--- Scala With Cats/src/main/kotlin/ch04/06_ReaderMonad.kt
+++ Scala With Cats/src/main/kotlin/ch04/06_ReaderMonad.kt
@@ -1,3 +1,117 @@
 package ch04
 
+import arrow.core.Id
+import arrow.core.Option
+import arrow.core.getOrElse
+import arrow.data.Reader
+import arrow.data.fix
+import arrow.data.flatMap
+import arrow.data.map
+import arrow.instances.monad
+import arrow.typeclasses.binding
+import p
+
+
 // 4.8 The Reader Monad
+// sequence operations that depend on input. Instances of reader wrap up functions of one argument, providing methods to
+// compose them
+
+// Ex dependency injection - a number of operations athat depend on external config, chained together using a reader to produc
+// one large operation accepts the config as a param and runs our porgram in the order specified
+data class Cat(val name: String, val food: String)
+
+fun creatingUnpackingReaders() {
+//   Reader<A,B> from function A->B
+
+
+  // not the id wrapped value, see:
+  // https://gitter.im/arrow-kt/Lobby?at=5af2dd7e1eddba3d04e402c8
+  val catName: Reader<Cat, String> = Reader { cat -> Id(cat.name) }
+  val catNameRun = catName.run { Cat("name", "food") }
+
+  catName.p() // Kleisli
+  catNameRun.p() // Cat(name=name,food=food)
+}
+
+fun composingReaders() {
+  // why readers, because of map and flatmap methods respersent different kinds of function composition
+  // create a set of readers that accept the same type of config, combine them with map and fm then call
+  // run to injec the config at the end
+  // map extends the computation in the reader by passing its result through a function
+
+  val theCat = Cat("ScoobyDoo", "Food")
+
+  val catName: Reader<Cat, String> = Reader { cat -> Id(cat.name) }
+  val exclaim: Reader<Cat, String> = catName.map { s -> s.plus("!") }
+
+  exclaim.run(theCat).p() // id(value=A)
+
+  // flatmap is more interesting - we can combine readers that depend on same input type
+
+  val feed: Reader<Cat, String> = Reader { cat -> Id("Feeding ${cat.food}") }
+
+  // not right
+//  val ef= exclaim.andThen { ex -> feed.map{ fe -> fe.plus(ex)} }
+
+  val eff = exclaim.flatMap { ex -> feed.map { fe -> fe.plus(" to ").plus(ex) } }
+  eff.run(theCat).p()
+
+  val m = Reader().monad<Cat>().binding {
+    feed.bind().plus(" to ").plus(exclaim.bind())
+  }
+
+  m.fix().run(theCat.copy("Felix")).p()
+}
+
+// classic use of readers to build a program that accepts a config as a param like login system
+// two db, a list of valid users and list of password
+
+// create a type alias DbReader for a reader that consumes a db as input
+class Db(val usernames: Map<Int, String>, val passwords: Map<String, String>)
+typealias DbReader<A> = Reader<Db, A>
+
+fun hackingReaders() {
+
+  fun findUsername(userId: Int): DbReader<Option<String>> =
+    Reader { db -> Id(Option.fromNullable(db.usernames[userId])) }
+
+  fun checkPassword(username: String, password: String): DbReader<Boolean> =
+    Reader { Id(it.passwords[username].equals(password)) }
+
+  fun checkLogin(userId: Int, password: String): DbReader<Boolean> =
+    findUsername(userId).flatMap {
+      it.map { username -> checkPassword(username, password) }
+        .getOrElse { DbReader { Id(false) } }
+    }
+
+  // could not get syntax version to work
+//  fun checkLogin1(userId: Int, password: String)/*: DbReader<Boolean>*/ =
+//    Reader().applicative<Boolean>().just(false)
+//  Reader { Id(it.passwords[it.usernames[userId]].equals(password)) }
+
+  val users = mapOf(1 to "a", 2 to "b", 3 to "c")
+  val passwords = mapOf("a" to "aa", "b" to "bb", "c" to "cc")
+  val db = Db(users, passwords)
+
+  checkLogin(1, "aa").run(db).p()
+  checkLogin(4, "aa").run(db).p()
+
+
+}
+
+fun main(args: Array<String>) {
+//  creatingUnpackingReaders()
+//  composingReaders()
+  hackingReaders()
+}
+
+// 4.8.4 when to use readers - tool for dependency injection, write steps of porgram as instances of reader, chain them
+// together with map and flatmap and build a function that accepts the dependency as input
+
+// usefule in sutuations where - we are constructing a batch program that can be represented by a function
+// need to defer injection of a known param or set of params
+// want to be able to test parts of the program in isolation
+
+// for more advanced problems with lots of dependencies, or not easly reprresneted as a pure function other dep injctions better
+
+// Kleisli Arrows - provide a more general form of reader generalises over type constructor of the result type
